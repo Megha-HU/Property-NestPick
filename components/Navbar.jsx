fa6693ddf3ec6c4ae926298bc7453306a8b1ef41
Megna_mahesh <megna.mahesh31@gmail.com>
2025-06-25 00:29:35 +0200

    fixed login button issue for mobile

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -226,13 +226,21 @@ const NavBar = () => {
                   Add Property
                 </Link>
               )}
-              {!session || providers &&
+            {!session && (
+              <div>
+                  {providers &&
                     Object.values(providers).map((provider, index) => (
-                <button   onClick={() => signIn(provider.id)} className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
-                  <i className="fa-brands fa-google mr-2"></i>
-                  <span>Login or Register</span>
-                </button>
-              ))}
+                      <button
+                        key={index}
+                        onClick={() => signIn(provider.id)}
+                        className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
+                      >
+                         <i className="fa-brands fa-google mr-2"></i>
+                        <span>Login or Register</span>
+                      </button>
+                    ))}
+              </div>
+            )}
             </div>
           </div>
         )}
